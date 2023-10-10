85a2c39515269440a25c852bb224ebab08c8d85e
kiwihodl <orangeamber@protonmail.com>
2023-10-10 13:59:44 -0500

    updating the footer

--- lhfhomes/src/App.js
+++ lhfhomes/src/App.js
@@ -1,7 +1,6 @@
 import Navbar from "./components/navbar/Navbar";
 import About from './components/about/About';
 import Carousel, { CarouselItem } from "./components/carousel/Carousel";
-// import Services from "./components/services/Services";
 import Footer from "./components/footer/Footer"
 import './App.css'
 
--- lhfhomes/src/components/footer/Footer.js
+++ lhfhomes/src/components/footer/Footer.js
@@ -1,53 +1,175 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import Link from '@mui/material/Link'; 
-
-import './footer.css';
+import React from "react";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import "./footer.css";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const Footer = () => {
+  const virtualTourStyle = {
+    fontFamily: "'Lobster Two', cursive",
+    fontSize: "63px",
+    color: "#FFF5E1",
+    paddingBottom: "33px",
+  };
+
+  const isMobile = useMediaQuery("(max-width: 900px)");
+
   return (
-    <AppBar position="static" color="primary" classes={{ root: 'matte-black-footer' }}>
-      <Toolbar sx={{ paddingTop: '20px', paddingBottom: '20px', backgroundColor: '#333' }}>
+    <AppBar
+      position="static"
+      color="primary"
+      classes={{ root: "matte-black-footer" }}
+    >
+      <Toolbar
+        sx={{
+          paddingTop: "20px",
+          paddingBottom: "20px",
+          backgroundColor: "#333",
+        }}
+      >
         <Box
           display="flex"
           flexDirection="column"
           alignItems="center"
           width="100%"
+          id="contact"
         >
-          <Typography variant="h5">
-                Our Mission: To model Jesus' love by creating life-enhancing relationships, services, and environments.
+          <Typography variant="h3" style={virtualTourStyle}>
+            Contact Us
           </Typography>
-          <Typography variant="h6">Contact Us</Typography>
-          <Box marginTop={2}>
-            <IconButton color="inherit" sx={{ color: '#F7931D', '&:hover': { backgroundColor: '#FFA040', color: '#333' }}} href="https://www.facebook.com">
-              <FacebookIcon />
-            </IconButton>
-            <IconButton color="inherit" sx={{ color: '#F7931D', '&:hover': { backgroundColor: '#FFA040', color: '#333' }}} href="https://www.instagram.com">
-              <InstagramIcon />
-            </IconButton>
-            <IconButton color="inherit" sx={{ color: '#F7931D', '&:hover': { backgroundColor: '#FFA040', color: '#333' }}} href="mailto:example@example.com">
-              <MailOutlineIcon />
-            </IconButton>
-          </Box>
           <Typography variant="h6">
             5595 County Road Z, West Bend, WI 53095 | (262) 306-2100
           </Typography>
           <Typography variant="h6">
             Copyright © 2023 Cedar Community // All rights reserved.
           </Typography>
-          <Link href="https://example.com" target="_blank" rel="noopener">
-            Terms and Conditions
-          </Link>
-          <Link href="https://example.com" target="_blank" rel="noopener">
-            Privacy Policy
-          </Link>
+          <Typography variant="h6">Contact Details</Typography>
+
+          <Grid container spacing={2} justifyContent="center">
+            {/* Left Side */}
+            <Grid
+              item
+              xs={12}
+              md={6}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: isMobile ? "center" : "flex-end",
+                justifyContent: isMobile ? "center" : "flex-end", 
+                textAlign: "center",
+              }}
+            >
+              <Typography variant="h6">Charm</Typography>
+              <Typography variant="body1">charm@asi-wi.com</Typography>
+              <Typography variant="body1">2422123435</Typography>
+            </Grid>
+            {/* Right Side */}
+            <Grid
+              item
+              xs={12}
+              md={6}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: isMobile ? "center" : "flex-start",
+                justifyContent: isMobile ? "center" : "flex-start",
+                textAlign: "center",
+              }}
+            >
+              <Typography variant="h6">Charm</Typography>
+              <Typography variant="body1">charm@asi-wi.com</Typography>
+              <Typography variant="body1">2422123435</Typography>
+            </Grid>
+          </Grid>
+
+          <Box marginTop={2}>
+            <IconButton
+              color="inherit"
+              sx={{
+                color: "#F7931D",
+                "&:hover": { backgroundColor: "#FFA040", color: "#333" },
+              }}
+              href="https://www.facebook.com"
+            >
+              <FacebookIcon />
+            </IconButton>
+            <IconButton
+              color="inherit"
+              sx={{
+                color: "#F7931D",
+                "&:hover": { backgroundColor: "#FFA040", color: "#333" },
+              }}
+              href="https://www.instagram.com"
+            >
+              <InstagramIcon />
+            </IconButton>
+            <IconButton
+              color="inherit"
+              sx={{
+                color: "#F7931D",
+                "&:hover": { backgroundColor: "#FFA040", color: "#333" },
+              }}
+              href="mailto:example@example.com"
+            >
+              <MailOutlineIcon />
+            </IconButton>
+          </Box>
+
+          <Typography variant="h6">Links</Typography>
+
+          <Grid container spacing={2} justifyContent="center">
+            {/* Left Side */}
+            <Grid
+              item
+              xs={12}
+              md={6}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: isMobile ? "center" : "flex-end",
+                justifyContent: isMobile ? "center" : "flex-end", 
+              }}
+            >
+              <Link
+                href="https://example.com"
+                target="_blank"
+                rel="noopener"
+                className="FooterLink"
+              >
+                Privacy Policy
+              </Link>
+            </Grid>
+            {/* Right Side */}
+            <Grid
+              item
+              xs={12}
+              md={6}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: isMobile ? "center" : "flex-start",
+                justifyContent: isMobile ? "center" : "flex-start",
+                textAlign: "center",
+              }}
+            >
+              <Link
+                href="https://example.com"
+                target="_blank"
+                rel="noopener"
+                className="FooterLink"
+              >
+                Terms and Conditions
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
       </Toolbar>
     </AppBar>
