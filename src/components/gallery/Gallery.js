7c3a2c747cf2ed4d86e6afcf280369cd62f1624b
kiwihodl <orangeamber@protonmail.com>
2023-10-12 20:18:43 -0500

    fixing video and hopefully the meta links for sharing

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,3 +1,99 @@
+// import React from "react";
+// import { Grid, Card, CardContent, Typography } from "@mui/material";
+// import ImagesCarousel from "../carousel/Carousel";
+// import logo from "../../assets/img/LHFHLogoTransparent.png";
+
+// const Gallery = () => {
+//   const containerStyle = {
+//     backgroundColor: "#333",
+//     textAlign: "center",
+//     padding: "40px 0",
+//   };
+
+//   const logoStyle = {
+//     maxWidth: "100px",
+//     height: "auto",
+//     marginBottom: "16px",
+//   };
+
+//   const virtualTourStyle = {
+//     fontFamily: "'Lobster Two', cursive",
+//     fontSize: "63px",
+//     color: "#FFF5E1",
+//     paddingBottom: "33px",
+//   };
+
+//   const galleryContainerStyle = {
+//     backgroundColor: "#FFF5E1",
+//     border: "2px solid #F7931D",
+//     borderRadius: "15px",
+//     textAlign: "center",
+//     padding: "20px",
+//     marginTop: "20px",
+//     minHeight: "700px",
+//     width: "80%",
+//     margin: "0 auto",
+//     display: "flex",
+//     flexDirection: "column",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     "@media (max-width: 600px)": {
+//       minHeight: "300px",
+//     },
+//   };
+
+//   const cardStyle = {
+//     width: "100%",
+//     height: "600px",
+//     border: "2px solid #F7931D",
+//     borderRadius: "15px",
+//     display: "flex",
+//     flexDirection: "column",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     maxWidth: "800px",
+//     margin: "0 auto",
+//     backgroundColor: "#333",
+//   };
+
+//   const cardContentStyle = {
+//     textAlign: "center",
+//     color: "#FFF5E1",
+//     padding: "20px",
+//     backgroundColor: "#333",
+//   };
+
+//   return (
+//     <div style={containerStyle} id="tour">
+//       <img src={logo} alt="Love Hope Faith Homes Logo" style={logoStyle} />
+//       <Typography variant="h6" style={virtualTourStyle}>
+//         Virtual Tour
+//       </Typography>
+//       <div style={galleryContainerStyle}>
+//         <Grid container spacing={2} justifyContent="center" alignItems="center">
+//           <Grid item xs={12} md={6}>
+//             <div style={cardStyle}>
+//               <div style={cardContentStyle}>
+//                 <video src="https://www.youtube.com/embed/IrAfDr7w-SM" />
+//                 <Typography variant="h6">Placeholder</Typography>
+//               </div>
+//             </div>
+//           </Grid>
+//           <Grid item xs={12} md={6}>
+//             <Card style={cardStyle}>
+//               <CardContent style={cardContentStyle}>
+//                 <ImagesCarousel />
+//               </CardContent>
+//             </Card>
+//           </Grid>
+//         </Grid>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Gallery;
+
 import React from "react";
 import { Grid, Card, CardContent, Typography } from "@mui/material";
 import ImagesCarousel from "../carousel/Carousel";
@@ -54,6 +150,7 @@ const Gallery = () => {
     maxWidth: "800px",
     margin: "0 auto",
     backgroundColor: "#333",
+    overflow: "hidden"
   };
 
   const cardContentStyle = {
@@ -61,22 +158,6 @@ const Gallery = () => {
     color: "#FFF5E1",
     padding: "20px",
     backgroundColor: "#333",
-  };
-
-  const galleryContainerMobileStyle = {
-    backgroundColor: "#FFF5E1",
-    border: "2px solid #F7931D",
-    borderRadius: "15px",
-    textAlign: "center",
-    padding: "20px",
-    marginTop: "20px",
-    minHeight: "700px",
-    width: "80%",
-    margin: "0 auto",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
   };
 
   return (
@@ -88,12 +169,19 @@ const Gallery = () => {
       <div style={galleryContainerStyle}>
         <Grid container spacing={2} justifyContent="center" alignItems="center">
           <Grid item xs={12} md={6}>
-            <Card style={cardStyle}>
-              <CardContent style={cardContentStyle}>
-                {/* <Video /> */}
-                <Typography variant="h6">Placeholder</Typography>
-              </CardContent>
-            </Card>
+            <div style={cardStyle}>
+              <div style={cardContentStyle}>
+                <iframe
+                  width="853"
+                  height="480"
+                  src="https://www.youtube.com/embed/IrAfDr7w-SM"
+                  title="Embedded youtube"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                />
+              </div>
+            </div>
           </Grid>
           <Grid item xs={12} md={6}>
             <Card style={cardStyle}>
--- src/components/youtubeEmbed/YoutubeEmbed.js
+++ src/components/youtubeEmbed/YoutubeEmbed.js
@@ -1,13 +1,12 @@
 import React from "react";
-import PropTypes from "prop-types";
 import "./youtubeembed.css";
 
-const YoutubeEmbed = ({ embedId }) => (
+const YoutubeEmbed = () => (
   <div className="video-responsive">
     <iframe
-      width="853"
-      height="480"
-      src={`https://www.youtube.com/embed/${embedId}`}
+      // width="100%"
+      // height="100%"
+      src={`https://www.youtube.com/embed/IrAfDr7w-SM}`}
     //   frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
@@ -15,9 +14,5 @@ const YoutubeEmbed = ({ embedId }) => (
     />
   </div>
 );
-
-YoutubeEmbed.propTypes = {
-  embedId: PropTypes.string.isRequired
-};
 
 export default YoutubeEmbed;
